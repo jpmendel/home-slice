d693ea7e24719aa3be82739898607fbfa0cefcef
Jacob Mendelowitz <jmdev37@gmail.com>
2024-04-12 17:11:40 -0400

    Add webserver configs.

---
+++ deploy/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -o allexport
+source .pienv
+set +o allexport
+
+echo "Copying to Raspberry Pi..."
+rsync -avz --rsh=ssh . "${RPI_REMOTE_USER}@${RPI_REMOTE_HOST}:/home/pi/app" \
+    --include-from=./deploy/sync_files.txt --delete-excluded
+echo "App Uploaded!"
