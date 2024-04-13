ade7eb34b5fc80d211fedaf13270e3570c391ef7
Jacob Mendelowitz <jmdev37@gmail.com>
2024-04-13 12:01:38 -0400

    Update configs and prod deps.

--- deploy/update.sh
+++ deploy/update.sh
@@ -5,6 +5,5 @@ source .pienv
 set +o allexport
 
 echo "Copying to Raspberry Pi..."
-rsync -avz --rsh=ssh . "${RPI_REMOTE_USER}@${RPI_REMOTE_HOST}:/home/pi/app" \
-    --include-from=./deploy/sync_files.txt --delete-excluded
+rsync -avz --rsh=ssh . "${RPI_REMOTE_USER}@${RPI_REMOTE_HOST}:/home/pi/app" --include-from=./deploy/sync_files.txt
 echo "App Uploaded!"
